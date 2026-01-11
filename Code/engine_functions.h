8e17ba4d468dc8434ef107f257908e8a4700988c
Villemoi <ville.moilanen1@gmail.com>
2026-01-11 20:10:23 +0200

    cleanup and readme adjustments

--- Code/engine_functions.h
+++ Code/engine_functions.h
@@ -5,25 +5,18 @@
 #include <SDL3/SDL_video.h>
 #include "../Classes/scene.h"
 
-// From start.cpp
-Scene LoadScene(SDL_Renderer* renderer); //
+Scene LoadScene(SDL_Renderer* renderer);
 
-// From loop.cpp
-int Loop(int argc, char* argv[]); //
+int Loop(int argc, char* argv[]);
 
-// From render.cpp
-void Render(SDL_Renderer* renderer, Scene& scene); //
+void Render(SDL_Renderer* renderer, Scene& scene);
 
-// From calculations.cpp
-// Note: Changed to pass by reference (Scene& scene) to fix the logic bug
-void Calculations(Scene& scene, float deltaTime); //
+void Calculations(Scene& scene, float deltaTime);
 
-// From inputs.cpp
 namespace Input {
     bool IsKeyDown(SDL_Scancode key);
 }
 
-// From shutdown.cpp
-void Shutdown(Scene& scene, SDL_Window* window); //
+void Shutdown(Scene& scene, SDL_Window* window);
 
 #endif
--- README.md
+++ README.md
@@ -4,13 +4,9 @@
 # Build and run
 ## Build 
 on VS Code: 
->open main.cpp
-
 >press Ctrl+Shift+B (Linux) or Cmd+Shift+B (Mac)
 ## Run 
 on VS Code: 
->open main.cpp
-
 >press Ctrl+Shift+P (Linux) or Cmd+Shift+P (Mac)
 
 >Select "Tasks: Run Task"
