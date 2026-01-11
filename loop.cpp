2cfc413c591d3d157f87a4dd9bc9d7b1a14466c1
Villemoi <ville.moilanen1@gmail.com>
2026-01-11 15:52:58 +0200

    loop cleanup

--- loop.cpp
+++ loop.cpp
@@ -1,5 +1,6 @@
 #include "classes.h"
 #include "inputs.cpp"
+#include "render.cpp"
 #include "calculations.cpp"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -25,27 +26,18 @@ int loop(skene skene, int argc, char* argv[]) {
     int counter = 0;
 
     while (running) {
-        /*
-        //inputit
-        inputs();
-        //peli laskut
-        calculations(counter, skene, running);
-        */
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 running = false;
             }
         }
+
+        //logiikka
+        calculations(skene);
+
         //kuvan renderöinti
-
-        // 1. Set the draw color (R, G, B, A) -> Let's go with a dark red
-        SDL_SetRenderDrawColor(renderer, 150, 20, 20, 255);
-
-        // 2. Clear the screen with that color
-        SDL_RenderClear(renderer);
-
-        // 3. Present the new frame to the window
-        SDL_RenderPresent(renderer);
+        render(renderer);
     }
 
     SDL_DestroyWindow(window);
